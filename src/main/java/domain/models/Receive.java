73eab20e5d5c9b36a8a4606cf9911fd06663dec4
21lucasmessias <21.lucasmessias@gmail.com>
2022-04-25 19:56:37 -0300

    feat: add models

---
+++ src/main/java/domain/models/Receive.java
@@ -0,0 +1,8 @@
+package domain.models;
+
+public class Receive {
+    private Number quantity;
+    private String idDonation;
+    private String idReceiver;
+
+}
---
+++ src/main/java/domain/models/User.java
@@ -0,0 +1,58 @@
+package domain.models;
+
+public class User {
+
+    private String id;
+    private String cpf;
+    private String password;
+    private String name;
+    private String phone;
+
+    public User(String id, String cpf, String password, String name, String phone) {
+        this.id = id;
+        this.cpf = cpf;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
